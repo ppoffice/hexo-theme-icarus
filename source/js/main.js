bcabdd87dd0150b16dc9e94fab7436d0ec62089d
ppoffice <ppoffice_2008@163.com>
2016-04-23 14:13:48 +0800

    fix: comment counter issue & no document scroll listening when no sidebar

--- source/js/main.js
+++ source/js/main.js
@@ -33,20 +33,22 @@
     });
 
     // To Top
-    $(document).on('scroll', function () {
-        if ($(document).width() >= 800) {
-            if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {    
-                $('#toTop').fadeIn();
-                $('#toTop').css('left', $('#sidebar').offset().left);
+    if ($('#sidebar').length) {
+        $(document).on('scroll', function () {
+            if ($(document).width() >= 800) {
+                if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
+                    $('#toTop').fadeIn();
+                    $('#toTop').css('left', $('#sidebar').offset().left);
+                } else {
+                    $('#toTop').fadeOut();
+                }
             } else {
-                $('#toTop').fadeOut();
+                $('#toTop').fadeIn();
+                $('#toTop').css('right', 20);
             }
-        } else {
-            $('#toTop').fadeIn();
-            $('#toTop').css('right', 20);
-        }
-    }).on('click', '#toTop', function () {
-        $('body, html').animate({ scrollTop: 0 }, 600);
-    });
+        }).on('click', '#toTop', function () {
+            $('body, html').animate({ scrollTop: 0 }, 600);
+        });
+    }
 
 })(jQuery);
