2f09a495d9fa08f23289be46d0f3b66c0ca9bf07
butlerx <cianbutlerx@gmail.com>
2016-08-29 01:01:09 +0100

    finish contact form

--- source/js/contact.js
+++ source/js/contact.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   var email, name, subject, text;
   var to = window.location.pathname;
-  $("#contact").submit(function() {
-      email=$("#email").val();
-      name=$("#name").val().trim();
-      subject=$("#subject").val().trim();
-      text=$("#content").val().trim();
+  to = to.substring(to.lastIndexOf('/')+1)
+  $("#contact").submit(function(e) {
+    if(grecaptcha.getResponse() == "") {
+      e.preventDefault();
+      $("#message").empty().html("complete recaptcha");
+    } else {
+      email = $("#email").val();
+      name = $("#fullname").val().trim();
+      subject = $("#subject").val().trim();
+      text = $("#content").val().trim();
       $("#message").text("Sending E-mail...Please wait");
       $.get("http://localhost:3000/api/contact", { email:email, to:to, name:name, subject:subject, text:text }, function(data) {
-      if(data=="sent") {
-        $("#message").empty().html("Email is been sent");
-      }
-    });
+        if(data=="sent") {
+          $("#message").empty().html("Email is been sent");
+        }
+      });
+    }
   });
 });
