d2b4e6a339bdb72ca4ee768fdb02f496e49b2de2
ppoffice <ppoffice_2008@163.com>
2016-10-20 21:54:40 +0800

    fix: justified gallery config name

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@
     // Caption
     $('.article-entry').each(function(i) {
         $(this).find('img').each(function() {
-            if (this.alt) {
+            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
                 $(this).after('<span class="caption">' + this.alt + '</span>');
             }
 
