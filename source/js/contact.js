ea649fdae4d9a31081946da400b73dd131382453
Cian Butler <butlerx@notthe.cloud>
2017-02-13 16:29:42 +0100

    fix wrong var (#41)

--- source/js/contact.js
+++ source/js/contact.js
@@ -10,17 +10,17 @@ $(document).ready(function(){
       $('#message').empty().html('complete recaptcha');
     } else {
       e.preventDefault();
-      email = $("#email").val();
-      name = $("#fullname").val().trim();
-      subject = $("#subject").val().trim();
-      text = $("#content").val().trim();
-      $("#message").text("Sending E-mail");
-      $.get(site + "/api/contact", { email:email, to:to, name:name, subject:subject, text:text, recaptcha:recaptcha}, function(data) {
+      email = $('#email').val();
+      name = $('#fullname').val().trim();
+      subject = $('#subject').val().trim();
+      text = $('#content').val().trim();
+      $('#message').text('Sending E-mail');
+      $.get(site + '/api/contact', { email:email, to:to, name:name, subject:subject, text:text, recaptcha:recaptcha}, function(data) {
         if(!data.formSubmit) {
-          $("#message").empty().html('Error in recaptcha');
+          $('#message').empty().html('Error in recaptcha');
         } else {
-          const message = data.error ? data.error : 'Email Sent';
-          $("#message").empty().html(message);
+          const message = data.errors ? data.errors : 'Email Sent';
+          $('#message').empty().html(message);
         }
       });
     }
