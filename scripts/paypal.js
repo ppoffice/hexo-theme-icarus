b6381b1759ed6ed62b54314e031df1ab23507c44
Cian Butler <butlerx@notthe.cloud>
2017-04-28 21:16:13 +0100

    add paypal code block (#45)
    * add paypal code block
    
    * replace table with div

---
+++ scripts/paypal.js
@@ -0,0 +1,9 @@
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
+
+hexo.extend.tag.register('paypal', () => {
+  const htmlTmlSrc = path.join(__dirname, '..', 'layout', 'paypal.ejs');
+  const htmlTml = ejs.compile(fs.readFileSync(htmlTmlSrc, 'utf-8'));
+  return htmlTml({});
+})
