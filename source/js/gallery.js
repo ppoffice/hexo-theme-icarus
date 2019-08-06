d150e555ce3630058fa6fa39d295396706b39a93
ppoffice <zrp1994@gmail.com>
2019-08-05 23:54:24 -0400

    fix(gallery): unwrap <p> for justified gallery to work

--- source/js/gallery.js
+++ source/js/gallery.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
         $('.article').lightGallery({ selector: '.gallery-item' });
     }
     if (typeof ($.fn.justifiedGallery) === 'function') {
+        if ($('.justified-gallery > p > .gallery-item').length) {
+            $('.justified-gallery > p > .gallery-item').unwrap();
+        }
         $('.justified-gallery').justifiedGallery();
     }
 });
