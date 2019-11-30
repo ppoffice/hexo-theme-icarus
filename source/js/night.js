38c96083f1351b5d43954d2f28574a39539b52da
iMaeGoo <mail1st@qq.com>
2019-11-30 10:59:56 +0800

    support night mode

---
+++ source/js/night.js
@@ -0,0 +1,32 @@
+/**
+ * Icarus 夜间模式 by iMaeGoo
+ * https://www.imaegoo.com/
+ */ 
+var isNight = localStorage.getItem('night');
+var nightNav;
+
+function applyNight(value) {
+    if (value.toString() === 'true') {
+        document.body.classList.add('night');
+    } else {
+        document.body.classList.remove('night');
+    }
+}
+
+function findNightNav() {
+    nightNav = document.getElementById('night-nav');
+    if (!nightNav) {
+        setTimeout(findNightNav, 100);
+    } else {
+        nightNav.addEventListener('click', switchNight);
+    }
+}
+
+function switchNight() {
+    isNight = isNight ? isNight.toString() !== 'true' : true;
+    applyNight(isNight);
+    localStorage.setItem('night', isNight);
+}
+
+findNightNav();
+applyNight(isNight);
