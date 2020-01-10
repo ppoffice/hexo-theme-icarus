26803f38072f60e01a5c662222bf1146aa523aa8
iMaeGoo <mail1st@qq.com>
2020-01-11 00:36:55 +0800

    fix(widget): incorrect links href (#592)
    Recommendation links should link to correct URL.

--- layout/widget/links.jsx
+++ layout/widget/links.jsx
@@ -16,7 +16,7 @@ class Links extends Component {
                                 hostname = new URL(hostname).hostname;
                             } catch (e) { }
                             return <li>
-                                <a class="level is-mobile is-mobile" href="<%- links[i] %>" target="_blank" rel="noopener">
+                                <a class="level is-mobile is-mobile" href={links[i]} target="_blank" rel="noopener">
                                     <span class="level-left">
                                         <span class="level-item">{i}</span>
                                     </span>
