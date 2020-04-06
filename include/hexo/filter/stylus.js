9cd422842c9751a17784db3ae24ba4dce2cac0d2
ppoffice <zrp1994@gmail.com>
2020-04-06 17:08:59 -0400

    fix(style): bind fontcdn helper to template locals

--- include/hexo/filter/stylus.js
+++ include/hexo/filter/stylus.js
@@ -3,7 +3,7 @@
  */
 module.exports = function(hexo) {
     hexo.extend.filter.register('template_locals', locals => {
-        const fontcdn = hexo.extend.helper.get('fontcdn').bind(hexo);
+        const fontcdn = hexo.extend.helper.get('fontcdn').bind(locals);
         hexo.theme.config['@fontface'] = {
             default: fontcdn('Ubuntu:wght@400;600&family=Source+Code+Pro', 'css2'),
             cyberpunk: fontcdn('Oxanium:wght@300;400;600&family=Roboto+Mono', 'css2')
