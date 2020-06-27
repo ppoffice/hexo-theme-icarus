c738800fdaa5c617edc26221ceffcdbef1ee96ae
xu.xiaojing <xu.xiaojing@datasw.com>
2020-06-27 17:17:26 +0800

    add auto generate excerpt of article feature

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -1,5 +1,5 @@
 const moment = require('moment');
-const { Component, Fragment } = require('inferno');
+const {Component, Fragment} = require('inferno');
 const Share = require('./share');
 const Donates = require('./donates');
 const Comment = require('./comment');
@@ -16,11 +16,54 @@ function getWordCount(content) {
     return content ? (content.match(/[\u00ff-\uffff]|[a-zA-Z]+/g) || []).length : 0;
 }
 
+/**
+ * get the content of article : full text or summary
+ */
+function getArticleContent(index, page, config) {
+
+    if (index) {
+        /* more tag */
+        if (page.excerpt) {
+            return page.excerpt;
+        }
+        /* auto generate excerpt */
+        if (typeof (config.auto_excerpt) !== 'undefined' && config.auto_excerpt.enable) {
+            if (config.auto_excerpt.length <= 0) {
+                return "";
+            } else {
+                var position = page.content.indexOf('\n', 0);
+                while (position < page.content.length - 1 && position <= config.auto_excerpt.length - 1) {
+                    var newPosition = page.content.indexOf('\n', position + 1);
+                    if (newPosition <= 0) {
+                        break;
+                    } else {
+                        position = newPosition;
+                    }
+                }
+                return page.content.substring(0, position + 1);
+            }
+        }
+    }
+
+    return page.content;
+}
+
+/**
+ * determine if there is a summary
+ */
+function isEnableExcerpt(page, config) {
+
+    if (page.excerpt || (typeof (config.auto_excerpt) != 'undefined' && config.auto_excerpt.enable && config.auto_excerpt.length > 0)) {
+        return true;
+    }
+    return false;
+}
+
 module.exports = class extends Component {
     render() {
-        const { config, helper, page, index } = this.props;
-        const { article, plugins } = config;
-        const { has_thumbnail, get_thumbnail, url_for, date, date_xml, __, _p } = helper;
+        const {config, helper, page, index} = this.props;
+        const {article, plugins} = config;
+        const {has_thumbnail, get_thumbnail, url_for, date, date_xml, __, _p} = helper;
 
         const indexLaunguage = config.language || 'en';
         const language = page.lang || page.language || config.language || 'en';
@@ -31,17 +74,19 @@ module.exports = class extends Component {
                 {/* Thumbnail */}
                 {has_thumbnail(page) ? <div class="card-image">
                     {index ? <a href={url_for(page.link || page.path)} class="image is-7by3">
-                        <img class="thumbnail" src={get_thumbnail(page)} alt={page.title || get_thumbnail(page)} />
+                        <img class="thumbnail" src={get_thumbnail(page)} alt={page.title || get_thumbnail(page)}/>
                     </a> : <span class="image is-7by3">
-                        <img class="thumbnail" src={get_thumbnail(page)} alt={page.title || get_thumbnail(page)} />
+                        <img class="thumbnail" src={get_thumbnail(page)} alt={page.title || get_thumbnail(page)}/>
                     </span>}
                 </div> : null}
                 {/* Metadata */}
-                <article class={`card-content article${'direction' in page ? ' ' + page.direction : ''}`} role="article">
+                <article class={`card-content article${'direction' in page ? ' ' + page.direction : ''}`}
+                         role="article">
                     {page.layout !== 'page' ? <div class="article-meta size-small is-uppercase level is-mobile">
                         <div class="level-left">
                             {/* Date */}
-                            <time class="level-item" dateTime={date_xml(page.date)} title={date_xml(page.date)}>{date(page.date)}</time>
+                            <time class="level-item" dateTime={date_xml(page.date)}
+                                  title={date_xml(page.date)}>{date(page.date)}</time>
                             {/* author */}
                             {page.author ? <span class="level-item"> {page.author} </span> : null}
                             {/* Categories */}
@@ -49,7 +94,8 @@ module.exports = class extends Component {
                                 {(() => {
                                     const categories = [];
                                     page.categories.forEach((category, i) => {
-                                        categories.push(<a class="link-muted" href={url_for(category.path)}>{category.name}</a>);
+                                        categories.push(<a class="link-muted"
+                                                           href={url_for(category.path)}>{category.name}</a>);
                                         if (i < page.categories.length - 1) {
                                             categories.push(<span>&nbsp;/&nbsp;</span>);
                                         }
@@ -66,17 +112,20 @@ module.exports = class extends Component {
                                 })()}
                             </span> : null}
                             {/* Visitor counter */}
-                            {!index && plugins && plugins.busuanzi === true ? <span class="level-item" id="busuanzi_container_page_pv" dangerouslySetInnerHTML={{
-                                __html: '<i class="far fa-eye"></i>' + _p('plugin.visit', '&nbsp;&nbsp;<span id="busuanzi_value_page_pv">0</span>')
-                            }}></span> : null}
+                            {!index && plugins && plugins.busuanzi === true ?
+                                <span class="level-item" id="busuanzi_container_page_pv" dangerouslySetInnerHTML={{
+                                    __html: '<i class="far fa-eye"></i>' + _p('plugin.visit', '&nbsp;&nbsp;<span id="busuanzi_value_page_pv">0</span>')
+                                }}></span> : null}
                         </div>
                     </div> : null}
                     {/* Title */}
                     <h1 class="title is-3 is-size-4-mobile">
-                        {index ? <a class="link-muted" href={url_for(page.link || page.path)}>{page.title}</a> : page.title}
+                        {index ?
+                            <a class="link-muted" href={url_for(page.link || page.path)}>{page.title}</a> : page.title}
                     </h1>
                     {/* Content/Excerpt */}
-                    <div class="content" dangerouslySetInnerHTML={{ __html: index && page.excerpt ? page.excerpt : page.content }}></div>
+                    <div class="content"
+                         dangerouslySetInnerHTML={{__html: getArticleContent(index, page, config)}}></div>
                     {/* Tags */}
                     {!index && page.tags && page.tags.length ? <div class="article-tags size-small mb-4">
                         <span class="mr-2">#</span>
@@ -85,30 +134,33 @@ module.exports = class extends Component {
                         })}
                     </div> : null}
                     {/* "Read more" button */}
-                    {index && page.excerpt ? <a class="article-more button is-small size-small" href={`${url_for(page.path)}#more`}>{__('article.more')}</a> : null}
+                    {index && isEnableExcerpt(page, config) ? <a class="article-more button is-small size-small"
+                                                                 href={`${url_for(page.path)}#more`}>{__('article.more')}</a> : null}
                     {/* Share button */}
-                    {!index ? <Share config={config} page={page} helper={helper} /> : null}
+                    {!index ? <Share config={config} page={page} helper={helper}/> : null}
                 </article>
             </div>
             {/* Donate button */}
-            {!index ? <Donates config={config} helper={helper} /> : null}
+            {!index ? <Donates config={config} helper={helper}/> : null}
             {/* Post navigation */}
             {!index && (page.prev || page.next) ? <nav class="post-navigation mt-4 level is-mobile">
                 {page.prev ? <div class="level-start">
-                    <a class={`article-nav-prev level level-item${!page.prev ? ' is-hidden-mobile' : ''} link-muted`} href={url_for(page.prev.path)}>
+                    <a class={`article-nav-prev level level-item${!page.prev ? ' is-hidden-mobile' : ''} link-muted`}
+                       href={url_for(page.prev.path)}>
                         <i class="level-item fas fa-chevron-left"></i>
                         <span class="level-item">{page.prev.title}</span>
                     </a>
                 </div> : null}
                 {page.next ? <div class="level-end">
-                    <a class={`article-nav-next level level-item${!page.next ? ' is-hidden-mobile' : ''} link-muted`} href={url_for(page.next.path)}>
+                    <a class={`article-nav-next level level-item${!page.next ? ' is-hidden-mobile' : ''} link-muted`}
+                       href={url_for(page.next.path)}>
                         <span class="level-item">{page.next.title}</span>
                         <i class="level-item fas fa-chevron-right"></i>
                     </a>
                 </div> : null}
             </nav> : null}
             {/* Comment */}
-            {!index ? <Comment config={config} page={page} helper={helper} /> : null}
+            {!index ? <Comment config={config} page={page} helper={helper}/> : null}
         </Fragment>;
     }
 };
