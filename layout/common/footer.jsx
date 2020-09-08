f9eade019b8e5828cfb2f6f5e0de8e9c87d1f9b5
Robert Lu <robberphex@gmail.com>
2020-09-08 22:46:38 +0800

    show siteTitle if no logo

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -15,12 +15,23 @@ class Footer extends Component {
             visitorCounterTitle
         } = this.props;
 
+        let footerLogo = '';
+        if (logo) {
+            if (logo.text) {
+                footerLogo = logo.text;
+            } else {
+                footerLogo = <img src={logoUrl} alt={siteTitle} height="28" />;
+            }
+        } else {
+            footerLogo = siteTitle;
+        }
+
         return <footer class="footer">
             <div class="container">
                 <div class="level">
                     <div class="level-start">
                         <a class="footer-logo is-block mb-2" href={siteUrl}>
-                            {logo && logo.text ? logo.text : <img src={logoUrl} alt={siteTitle} height="28" />}
+                            {footerLogo}
                         </a>
                         <p class="size-small">
                             <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
