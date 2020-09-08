7784d964aa89c74671c9da3dbddd887404b659dd
Robert LU <robberphex@gmail.com>
2020-09-09 02:06:52 +0800

    show siteTitle if no logo (#785)

--- layout/common/navbar.jsx
+++ layout/common/navbar.jsx
@@ -28,11 +28,22 @@ class Navbar extends Component {
             searchTitle
         } = this.props;
 
+        let navbarLogo = '';
+        if (logo) {
+            if (logo.text) {
+                navbarLogo = logo.text;
+            } else {
+                navbarLogo = <img src={logoUrl} alt={siteTitle} height="28" />;
+            }
+        } else {
+            navbarLogo = siteTitle;
+        }
+
         return <nav class="navbar navbar-main">
             <div class="container">
                 <div class="navbar-brand justify-content-center">
                     <a class="navbar-item navbar-logo" href={siteUrl}>
-                        {logo && logo.text ? logo.text : <img src={logoUrl} alt={siteTitle} height="28" />}
+                        {navbarLogo}
                     </a>
                 </div>
                 <div class="navbar-menu">
