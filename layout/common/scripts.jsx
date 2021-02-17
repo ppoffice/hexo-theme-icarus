dd04afa433ba2c3cb3e1ca19df1a7559035575c7
七海 椎名 <ymd45921@163.com>
2021-02-17 12:54:50 +0800

    Fix no comment.jsUrl

--- layout/common/scripts.jsx
+++ layout/common/scripts.jsx
@@ -31,7 +31,7 @@ module.exports = class extends Component {
         return <Fragment>
             <script src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')}></script>
             <script src={cdn('moment', '2.22.2', 'min/moment-with-locales.min.js')}></script>
-            {config.comment.jsUrl && <script src={config.comment.jsUrl}></script>}
+            {config.comment?.jsUrl && <script src={config.comment.jsUrl}></script>}
             {clipboard && <script src={cdn('clipboard', '2.0.4', 'dist/clipboard.min.js')} defer></script>}
             <script dangerouslySetInnerHTML={{ __html: `moment.locale("${language}");` }}></script>
             <script dangerouslySetInnerHTML={{ __html: embeddedConfig }}></script>
