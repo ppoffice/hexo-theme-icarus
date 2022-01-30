da442f6c4ff43d5e38331aa48cb0117b0a644b2c
HMLTFan <xfqwdsj@qq.com>
2022-01-30 12:53:25 +0800

    Fixed some problems

--- scripts/tags/message.js
+++ scripts/tags/message.js
@@ -1,25 +1,26 @@
 /**
  * Bulma Message Tag, see {@link https://bulma.io/documentation/components/message/}.
- * 
+ *
  * @param {string} color    The color of this message, can not be set. Usable: dark, primary, link, info, success,
  *                          warning, danger.
  * @param {string} icon     The icon of this message, can not be set.
  * @param {string} title    The header of this message, can not be set, supported Markdown.
  * @param {string} size     The size of this message, can not be set. Usable: small, medium, large. The default
  *                          size is between small and medium.
- * 
+ *
  * @example
  * {% message color:danger icon:info-circle 'title:Very danger!' size:small %}
  *     **You are in danger.**
  * {% endmessage %}
  */
- module.exports = function (hexo) {
+module.exports = function(hexo) {
     hexo.extend.tag.register('message', function(args, content) {
         let color = 'dark';
         let icon = '';
         let title = '';
         let size = '';
         let header = '';
+
         args.forEach(element => {
             const key = element.split(':')[0].trim();
             const value = element.split(':')[1].trim();
@@ -45,8 +46,9 @@
             <div class="message-header">
                 ${hexo.render.renderSync({text: icon + title, engine: 'markdown'})}
             </div>
-            `
+            `;
         }
+
         return `
         <article class="message is-${color}${size}">
             ${header}
@@ -56,4 +58,4 @@
         </article>
         `;
     }, { ends: true });
-}
+};
--- scripts/tags/tabs.js
+++ scripts/tags/tabs.js
@@ -1,118 +1,123 @@
 /**
  * Bulma Tabs Tag, see {@link https://bulma.io/documentation/components/tabs/}.
- * 
+ *
  * The format of each item is: <!-- <active>item [id] [<icon>] '[title]' --> [content] <!-- enditem -->.
  * If each item's content is indented with four spaces or one tab, these indents will be ignored.
- * 
+ *
  * @param {string} id           The unique id of this tab, should match: /\w/.
  * @param {string} behavior     The behavior of this tab, can not be set. Usable: centered, right, fullwidth. The
  *                              default behavior is to display on the left.
  * @param {string} size         The size of this tab, can not be set. Usable: small, medium, large. The default
  *                              size is between small and medium.
  * @param {string} style        The style of this tab, can not be set. Usable: boxed, toggle, toggle-rounded.
- * 
+ *
  * @example
  * {% tabs behavior:fullwidth size:small style:toggle-rounded %}
  *     <!-- item info info 'Info' -->This is info.<!-- enditem -->
  *     <!-- activeitem hello 'Hello' -->This is hello.<!-- enditem -->
  * {% endmessage %}
  */
- hexo.extend.tag.register('tabs', function(args, content) {
-    let id = '';
-    let behavior = '';
-    let size = '';
-    let style = '';
-    let contentEl = content;
-    args.forEach(element => {
-        const key = element.split(':')[0].trim();
-        const value = element.split(':')[1].trim();
-        if (value !== null && value !== undefined && value !== '') {
-            switch (key) {
-                case 'id':
-                    id = value;
-                    break;
-                case 'behavior':
-                    behavior = ` is-${value}`;
-                    break;
-                case 'size':
-                    size = ` is-${value}`;
-                    break;
-                case 'style':
-                    if (value === 'toggle-rounded') {
-                        style = ' is-toggle is-toggle-rounded';
-                    } else {
-                        style = ` is-${value}`;
-                    }
-                    break;
+module.exports = function(hexo) {
+    hexo.extend.tag.register('tabs', function(args, content) {
+        let id = '';
+        let behavior = '';
+        let size = '';
+        let style = '';
+
+        args.forEach(element => {
+            const key = element.split(':')[0].trim();
+            const value = element.split(':')[1].trim();
+            if (value !== null && value !== undefined && value !== '') {
+                switch (key) {
+                    case 'id':
+                        id = value;
+                        break;
+                    case 'behavior':
+                        behavior = ` is-${value}`;
+                        break;
+                    case 'size':
+                        size = ` is-${value}`;
+                        break;
+                    case 'style':
+                        if (value === 'toggle-rounded') {
+                            style = ' is-toggle is-toggle-rounded';
+                        } else {
+                            style = ` is-${value}`;
+                        }
+                        break;
+                }
             }
-        }
-    });
+        });
 
-    const blockRegExp = /<!--\s*(active)?item( \w+)( \w+)?( '.*?')\s*-->([\s\S]*?)<!--\s*enditem\s*-->/g;
-    let match;
-    let tabsEl = '';
-    let contentEl = '';
+        const blockRegExp = /<!--\s*(active)?item( \w+)( \w+)?( '.*?')\s*-->([\s\S]*?)<!--\s*enditem\s*-->/g;
+        let match;
+        let tabsEl = '';
+        let contentEl = '';
 
-    while((match = blockRegExp.exec(content)) !== null) {
-        let active = '';
-        let hidden = ' is-hidden';
-        let icon = '';
-        var contentString = match[5].replace(/^\n?|[ \n\t]*$/g, '');
-        if (match[1] === 'active') {
-            active = ' class="is-active"';
-            hidden = '';
+        while((match = blockRegExp.exec(content)) !== null) {
+            let active = '';
+            let hidden = ' is-hidden';
+            let icon = '';
+            var contentString = match[5].replace(/^\n?|[ \n\t]*$/g, '');
+
+            if (match[1] === 'active') {
+                active = ' class="is-active"';
+                hidden = '';
+            }
+            if (match[3] === 'active' && match[3].substring(1) !== '') icon = `<span class="icon is-small"><i class="fas fa-${match[3].substring(1)}" aria-hidden="true"></i></span>`;
+            if (contentString.match(/^ {4}|^\t{1}/gm) !== null && contentString.match(/^ {4}|^\t{1}/gm).length === contentString.split('\n').length) contentString = contentString.replace(/^ {4}|^\t{1}/g, '').replace(/\n {4}|\n\t{1}/g, '\n');
+
+            tabsEl += `
+            <li id="${match[2].substring(1)}"${active}">
+                <a onclick="switchTab(this)">${hexo.render.renderSync({text: icon + match[4].substring(2, match[4].length - 1), engine: 'markdown'})}</a>
+            </li>
+            `;
+
+            contentEl += `
+            <div id="${match[2].substring(1)}" class="tab-content${hidden}">
+                ${hexo.render.renderSync({text: contentString, engine: 'markdown'})}
+            </div>
+            `;
         }
-        if (match[3] === 'active' && match[3].substring(1) !== '') icon = `<span class="icon is-small"><i class="fas fa-${match[3].substring(1)}" aria-hidden="true"></i></span>`;
-        if (contentString.match(/^ {4}|^\t{1}/gm) !== null && contentString.match(/^ {4}|^\t{1}/gm).length === contentString.split('\n').length) contentString = contentString.replace(/^ {4}|^\t{1}/g, '').replace(/\n {4}|\n\t{1}/g, '\n');
-        tabsEl += `
-        <li id="${match[2].substring(1)}"${active}">
-            <a onclick="switchTab(this)">${hexo.render.renderSync({text: icon + match[4].substring(2, match[4].length - 1), engine: 'markdown'})}</a>
-        </li>
-        `;
-        contentEl += `
-        <div id="${match[2].substring(1)}" class="tab-content${hidden}">
-            ${hexo.render.renderSync({text: contentString, engine: 'markdown'})}
-        </div>
-        `;
-    }
-    
-    return `
-    <div>
-        <div class="tabs my-3${behavior}${size}${style}">
-            <ul class="mx-0 my-0">
-                ${tabsEl}
-            </ul>
-        </div>
+
+        return `
         <div>
-            ${contentEl}
+            <div class="tabs my-3${behavior}${size}${style}">
+                <ul class="mx-0 my-0">
+                    ${tabsEl}
+                </ul>
+            </div>
+            <div>
+                ${contentEl}
+            </div>
         </div>
-    </div>
-    `;
-}, { ends: true });
+        `;
+    }, { ends: true });
 
-hexo.extend.injector.register(
-    "head_end",
-    `
-    <script>
-        function switchTab(element) {
-            const id = element.parentElement.id;
-            const tabElements = element.parentElement.parentElement.children;
-            const contentElements = element.parentElement.parentElement.parentElement.parentElement.children[1].children;
-            for (let i = 0; i < tabElements.length; i++) {
-                const $tab = tabElements[i];
-                const $content = contentElements[i];
-                if ($tab.id === id) {
-                    $tab.classList.add('is-active');
-                } else {
-                    $tab.classList.remove('is-active');
-                }
-                if ($content.id === id) {
-                    $content.classList.remove('is-hidden');
-                } else {
-                    $content.classList.add('is-hidden');
+    hexo.extend.injector.register(
+        "head_end",
+        `
+        <script>
+            function switchTab(element) {
+                const id = element.parentElement.id;
+                const tabElements = element.parentElement.parentElement.children;
+                const contentElements = element.parentElement.parentElement.parentElement.parentElement.children[1].children;
+                for (let i = 0; i < tabElements.length; i++) {
+                    const $tab = tabElements[i];
+                    const $content = contentElements[i];
+                    if ($tab.id === id) {
+                        $tab.classList.add('is-active');
+                    } else {
+                        $tab.classList.remove('is-active');
+                    }
+                    if ($content.id === id) {
+                        $content.classList.remove('is-hidden');
+                    } else {
+                        $content.classList.add('is-hidden');
+                    }
                 }
             }
-        }
-    </script>
-    `
-);
+        </script>
+        `
+    );
+};
