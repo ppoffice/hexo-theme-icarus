ae07ce35dc711eea5fa8ee1ff823833779ab0c83
Ruipeng Zhang <ppoffice@users.noreply.github.com>
2023-01-19 00:28:14 -0500

    Revert "feat(footer): added copyright"

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -11,7 +11,6 @@ class Footer extends Component {
             siteYear,
             author,
             links,
-            copyright,
             showVisitorCounter,
             visitorCounterTitle
         } = this.props;
@@ -42,7 +41,6 @@ class Footer extends Component {
                             {showVisitorCounter ? <span id="busuanzi_container_site_uv"
                                 dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span> : null}
                         </p>
-                        {copyright ? <p class="is-size-7" dangerouslySetInnerHTML={{ __html: copyright }}></p> : null}
                     </div>
                     <div class="level-end">
                         {Object.keys(links).length ? <div class="field has-addons">
@@ -86,7 +84,6 @@ module.exports = cacheComponent(Footer, 'common.footer', props => {
         siteYear: date(new Date(), 'YYYY'),
         author,
         links,
-        copyright: footer?.copyright ?? '',
         showVisitorCounter: plugins && plugins.busuanzi === true,
         visitorCounterTitle: _p('plugin.visitor_count', '<span id="busuanzi_value_site_uv">0</span>')
     };
