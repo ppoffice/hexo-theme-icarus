a434f2fff5e3d292def6561d0f83feb2fe9ef527
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-02-10 01:16:06 +0800

    Update head.jsx

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -165,10 +165,10 @@ module.exports = class extends Component {
             {canonical_url ? <link rel="canonical" href={canonical_url} /> : null}
             {rss ? <link rel="alternate" href={url_for(rss)} title={config.title} type="application/atom+xml" /> : null}
             {favicon ? <link rel="icon" href={url_for(favicon)} /> : null}
-            <link rel="stylesheet" href={iconcdn()} />
-            {hlTheme ? <link rel="stylesheet" href={cdn('highlight.js', '9.12.0', 'styles/' + hlTheme + '.css')} /> : null}
-            <link rel="stylesheet" href={fontCssUrl[variant]} />
-            <link rel="stylesheet" href={url_for('/css/' + variant + '.css')} />
+            <link rel="preload" as="style" href={iconcdn()} />
+            {hlTheme ? <link rel="preload" as="style" href={cdn('highlight.js', '9.12.0', 'styles/' + hlTheme + '.css')} /> : null}
+            <link rel="preload" as="style" href={fontCssUrl[variant]} />
+            <link rel="preload" as="style" href={url_for('/css/' + variant + '.css')} />
             <Plugins site={site} config={config} helper={helper} page={page} head={true} />
 
             {adsenseClientId ? <script data-ad-client={adsenseClientId}
