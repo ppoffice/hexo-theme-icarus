10f1c8627360a72f651ed18f7cdb46ff1980a6a7
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-02-25 16:24:24 +0800

    Update process.js

--- scripts/lazyload/lib/process.js
+++ scripts/lazyload/lib/process.js
@@ -1,6 +1,8 @@
+'use strict';
+const fs = require('hexo-fs');
 function lazyProcess(htmlContent) {
     let loadingImage = this.config.lazyload.loadingImg || 'https://img-blog.csdnimg.cn/2022010612032074818.gif';
-    return htmlContent.replace(/<img(\s+[^>]*?)?\s*alt="(.*?)"(.*?)\s*src="(.*?)"(.*?)>/gi, (str, p1, p2, p3, p4, p5) => {
+    return htmlContent.replace(/<img(\s*?)src="(.*?)"(.*?)alt="(.*?)"(.*?)>/gi, (str, p1, p2, p3, p4, p5) => {
         if (/data-src/gi.test(str)) {
             return str;
         }
@@ -8,16 +10,25 @@ function lazyProcess(htmlContent) {
             str = str.replace(/class="(.*?)"/gi, (classStr, p1) => {
                 return classStr.replace(p1, `${p1} lazyload`);
             })
-            return str.replace(p5, `${p5} srcset="${loadingImage}" data-srcset="${p4}"`);
+            return str.replace(p5, `${p5} srcset="${loadingImage}" data-srcset="${p2}"`);
         }
-        return str.replace(p5, `${p5} class="lazy" srcset="${loadingImage}" data-srcset="${p4}"`);
-    }).replace(/<img(\s+[^>]*?)?\s*src="(.*?)"(.*?)>/gi, (str, p1, p2, p3) => {
-        if (/\s*alt="(.*?)"/gi.test(str)) {
+        return str.replace(p5, `${p5} class="lazy" srcset="${loadingImage}" data-srcset="${p2}"`);
+    }).replace(/<img(\s*?)src="(.*?)"(.*?)>/gi, (str, p1, p2, p3) => {
+        if (/alt="(.*?)"/gi.test(str)) {
             return str;
         }
         if (/data-src/gi.test(str)) {
-            return str.replace(/<img/gi, '<img alt=""');
+            return str.replace(/(<img)(.*?)(>)/gi, '$1 alt="" $2 $3');
         }
         return str.replace(p3, `${p3} alt=""`);
     });
 }
+
+module.exports.processPost = function(data) {
+    data.content = lazyProcess.call(this, data.content);
+    return data;
+};
+
+module.exports.processSite = function (htmlContent) {
+    return lazyProcess.call(this, htmlContent);
+};
