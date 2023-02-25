08f3017ba78392af9b75ab284fb8dea934ec133c
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-02-26 00:19:09 +0800

    Update process.js

--- scripts/lazyload/lib/process.js
+++ scripts/lazyload/lib/process.js
@@ -1,19 +1,19 @@
 'use strict';
 const fs = require('hexo-fs');
-function lazyProcess(htmlContent) {
-  let loadingImage = this.config.lazyload.loadingImg || 'https://img-blog.csdnimg.cn/2022010612032074818.gif';
-  return htmlContent.replace(/<img(\s*?)src="(.*?)"(.*?)((\s*)alt="(.*?)")?>/gi, (str, p1, p2, p3, p4, p5, p6) => {
-    if (/data-src/gi.test(str)) {
-      return str;
-    }
-    if (/class="(.*?)"/gi.test(str)) {
-      str = str.replace(/class="(.*?)"/gi, (classStr, p1) => {
-        return classStr.replace(p1, `${p1} lazyload`);
-      })
-      return str.replace(p3, `${p3} srcset="${loadingImage}" data-srcset="${p2}"`);
-    }
-    return str.replace(p3, `${p3} class="lazy" srcset="${loadingImage}" data-srcset="${p2}"`);
-  });
+function lazyProcess(htmlContent)  {
+    let loadingImage = this.config.lazyload.loadingImg || 'https://cdn.jsdelivr.net/skx@0.0.9/img/lazy.gif';
+    return htmlContent.replace(/<img(\s*?)src="(.*?)"(.*?)>/gi, (str, p1, p2, p3)  =>  {
+        if (/data-src/gi.test(str)) {
+            return str;
+        }
+        if (/class="(.*?)"/gi.test(str)){
+            str = str.replace(/class="(.*?)"/gi, (classStr, p1) => {
+                return classStr.replace(p1, `${p1} lazyload`);
+            })
+            return str.replace(p3, `${p3} srcset="${loadingImage}" data-srcset="${p2}"`);
+        }
+        return str.replace(p3, `${p3} class="lazyload" srcset="${loadingImage}" data-srcset="${p2}"`);
+    });
 }
 
 module.exports.processPost = function(data) {
