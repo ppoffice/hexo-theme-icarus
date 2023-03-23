7eff12837613911cb4bd84dcff4e634c11b51909
xiang578 <xiang578@foxmail.com>
2023-03-24 07:39:55 +0800

    修改字体

--- scripts/injector.js
+++ scripts/injector.js
@@ -2,3 +2,5 @@ hexo.extend.filter.register('theme_inject', function (injects) {
     // 样式文件
 	injects.style.push('/source/_data/styles.styl');
 })
+
+hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.6.0/lxgwwenkaiscreen.css">', 'default');
