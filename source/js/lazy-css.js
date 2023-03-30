b3ea4797c58da9bbbb5c6700e64e9c2f477bbf88
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-03-30 10:50:15 +0800

    Update lazy-css.js

--- source/js/lazy-css.js
+++ source/js/lazy-css.js
@@ -1,23 +1,336 @@
-function Load() {
-  var runningOnBrowser = typeof window !== "undefined";
-  var isBot =
-    (runningOnBrowser && !("onscroll" in window)) ||
-    (typeof navigator !== "undefined" &&
-      /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(
-        navigator.userAgent
-      ));
-  var supportsIntersectionObserver =
-    runningOnBrowser && "IntersectionObserver" in window;
-  if (!isBot && supportsIntersectionObserver) {
-    var observer = new IntersectionObserver(
-      function (entries) {
-        if (entries[0].isIntersecting) {
-          observer.disconnect();
-        }
-      },
-      { threshold: [0] }
+importScripts('https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/workbox-sw/5.1.4/workbox-sw.min.js');
+
+workbox.setConfig({
+    modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/'
+});
+
+const { core, precaching, routing, strategies, expiration, cacheableResponse, backgroundSync } = workbox;
+const { CacheFirst, NetworkFirst, NetworkOnly,StaleWhileRevalidate } = strategies;
+const { ExpirationPlugin } = expiration;
+const { CacheableResponsePlugin } = cacheableResponse;
+
+const cacheSuffixVersion = '-210929a',
+    // precacheCacheName = core.cacheNames.precache,
+    // runtimeCacheName = core.cacheNames.runtime,
+    maxEntries = 100;
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => {
+                if (key.includes('disqus-cdn-cache')) return caches.delete(key);
+                if (key.includes('disqus-img-cache')) return caches.delete(key);
+                if (!key.includes(cacheSuffixVersion)) return caches.delete(key);
+            }));
+        })
     );
-    observer.observe(document.getElementById("vcomments"));
-  } else {
-  }
-}
+});
+
+
+core.setCacheNameDetails({
+    prefix: '186blog',
+    suffix: cacheSuffixVersion
+});
+
+core.skipWaiting();
+core.clientsClaim();
+precaching.cleanupOutdatedCaches();
+
+/*
+ * Cache File From jsDelivr
+ * cdn.jsdelivr.net | shadow.elemecdn.com
+ *
+ * Method: CacheFirst
+ * cacheName: static-immutable
+ * cacheTime: 30d
+ */
+
+// cdn.jsdelivr.net - cors enabled
+routing.registerRoute(
+    /.*lf3-cdn-tos\.bytecdntp\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+registerRoute(
+    /.*xgjalbum\.oss-cn-hangzhou\.aliyuncs\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*cdn\.staticfile\.org/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*littleskin\.cn/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*www\.bing\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*cdn\.nofated\.win/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*fonts\.googleapis\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*fonts\.gstatic\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*fonts\.gstatic\.cn/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    /.*www\.googletagmanager\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+routing.registerRoute(
+    new RegExp('https://www.google-analytics.com'),
+    new NetworkOnly({
+        plugins: [
+            new backgroundSync.BackgroundSyncPlugin('Google_Analytics', {
+                maxRetentionTime: 12 * 60
+            }),
+        ]
+    }),
+    "POST"
+);
+
+routing.registerRoute(
+    new RegExp('https://comment.9595095.xyz'),
+    new NetworkFirst({
+        plugins: [
+            new backgroundSync.BackgroundSyncPlugin('Comment', {
+                maxRetentionTime: 12 * 60
+            }),
+        ]
+    }),
+    "POST"
+);
+
+routing.registerRoute(
+    new RegExp('https://comment.9595095.xyz'),
+    new NetworkFirst({
+        plugins: [
+            new backgroundSync.BackgroundSyncPlugin('Comment_GET', {
+                maxRetentionTime: 12 * 60
+            }),
+        ]
+    }),
+    "GET"
+);
+
+routing.registerRoute(
+    new RegExp('https://analytics.google.com'),
+    new NetworkOnly({
+        plugins: [
+            new backgroundSync.BackgroundSyncPlugin('Google_Analytics_New', {
+                maxRetentionTime: 12 * 60
+            }),
+        ]
+    }),
+    "POST"
+)
+
+const StaleWhileRevalidateInstance = new StaleWhileRevalidate();
+/*
+ * Others img
+ * Method: staleWhileRevalidate
+ * cacheName: img-cache
+ */
+routing.registerRoute(
+    // Cache image files
+    /.*\.(?:png|jpg|jpeg|gif|webp)/,
+    StaleWhileRevalidateInstance
+);
+
+/*
+ * Static Assets
+ * Method: staleWhileRevalidate
+ * cacheName: static-assets-cache
+ */
+routing.registerRoute(
+    // Cache CSS files
+    /.*\.(css|js)/,
+    // Use cache but update in the background ASAP
+    StaleWhileRevalidateInstance
+);
+
+/*
+ * sw.js - Revalidate every time
+ * staleWhileRevalidate
+ */
+routing.registerRoute(
+    '/sw.js',
+    StaleWhileRevalidateInstance
+);
+
+
+
+routing.registerRoute(
+    /.*blog\.nofated\.win/,
+    StaleWhileRevalidateInstance
+);
+
+routing.registerRoute(
+    /.*blog\.nof\.moe/,
+    StaleWhileRevalidateInstance
+);
+
+
+
+routing.registerRoute(
+    /.*localhost/,
+    StaleWhileRevalidateInstance
+);
+
+
+routing.registerRoute(
+    /.*\.(?:svg)/,
+    new NetworkOnly()
+);
+
+routing.registerRoute(
+    /.*img-shields-io\.186526\.xyz/,
+    new NetworkOnly()
+);
+
+/*
+ * Default - Serve as it is
+ * StaleWhileRevalidate
+ */
+routing.setDefaultHandler(
+    new NetworkOnly()
+);>
