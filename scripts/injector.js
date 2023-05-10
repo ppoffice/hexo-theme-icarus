081aeb7a634e9c1dd33e68caf8d4a2699a758f95
xiang578 <xiang578@foxmail.com>
2023-05-10 20:43:06 +0800

    change name

--- scripts/injector.js
+++ scripts/injector.js
@@ -7,7 +7,7 @@ hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="https:
 
 hexo.extend.injector.register('head_begin', '<script async defer data-website-id="7e782528-455f-4f0a-acc5-01ec7ed759ff" src="https://analytics.umami.is/script.js"></script>', 'default');
 
-hexo.extend.injector.register('head_begin', '<script async src="<script async src="http://124.221.237.70:3000/script.js" data-website-id="0bf958fd-ca98-4a54-b1c0-bf3384802fcc"></script>', 'default');
+hexo.extend.injector.register('head_begin', '<script async src="https://umami.xiang578.com/script.js" data-website-id="0bf958fd-ca98-4a54-b1c0-bf3384802fcc"></script>', 'default');
 
 hexo.extend.injector.register('head_begin', '<link rel="webmention" href="https://webmention.io/blog.xiang578.com/webmention" /> <link rel="pingback" href="https://webmention.io/blog.xiang578.com/xmlrpc" />', 'default');
 hexo.extend.injector.register('head_begin', '<link title="Follow me on Twitter" rel="me" href="https://twitter.com/xiang578"/>', 'default');
