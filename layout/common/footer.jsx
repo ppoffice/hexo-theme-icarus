9108e0b0e3205024a819b3d22996c6173652fb8e
Nofated095 <nofated095@outlook.com>
2023-11-30 18:27:39 +0800

    update footer

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -38,6 +38,8 @@ class Footer extends Component {
                             <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
                             &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">Hexo</a>&nbsp;&&nbsp;
                             <a href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
+                            <br />
+                            Made with 💖 by <a href="https://nof.moe" target="_blank" rel="noopener">Nofated</a>
                             {showVisitorCounter ? <br /> : null}
                             {showVisitorCounter ? <span id="busuanzi_container_site_uv"
                                 dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span> : null}
