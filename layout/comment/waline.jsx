0eb6d6401db18c806d9fc30e124c108cf9697e1c
Nofated095 <nofated095@outlook.com>
2023-11-30 18:46:18 +0800

    fix waline require

--- layout/comment/waline.jsx
+++ layout/comment/waline.jsx
@@ -3,7 +3,7 @@
  * @module view/comment/waline
  */
 const { Component } = require('inferno');
-const { cacheComponent } = require('../../util/cache');
+const { cacheComponent } = require('hexo-component-inferno/lib/util/cache');
 
 /**
  * Waline comment JSX component.
