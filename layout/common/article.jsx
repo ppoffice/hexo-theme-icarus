49568ab9b08fba4e8702ffeef2a77c521a9d0544
ppoffice <8849362+ppoffice@users.noreply.github.com>
2024-01-05 20:56:15 -0800

    fix: use first site language in article layout when multiple languages configured

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -24,8 +24,10 @@ module.exports = class extends Component {
         const { article, plugins } = config;
         const { url_for, date, date_xml, __, _p } = helper;
 
-        const indexLaunguage = toMomentLocale(config.language || 'en');
-        const language = toMomentLocale(page.lang || page.language || config.language || 'en');
+        const defaultLanguage = Array.isArray(config.language) && config.language.length ? config.language[0] : config.language;
+
+        const indexLanguage = toMomentLocale(defaultLanguage || 'en');
+        const language = toMomentLocale(page.lang || page.language || defaultLanguage || 'en');
         const cover = page.cover ? url_for(page.cover) : null;
         const updateTime = article && article.update_time !== undefined ? article.update_time : true;
         const isUpdated = page.updated && !moment(page.date).isSame(moment(page.updated));
@@ -74,7 +76,7 @@ module.exports = class extends Component {
                                 {(() => {
                                     const words = getWordCount(page._content);
                                     const time = moment.duration((words / 150.0) * 60, 'seconds');
-                                    return `${_p('article.read_time', time.locale(index ? indexLaunguage : language).humanize())} (${_p('article.word_count', words)})`;
+                                    return `${_p('article.read_time', time.locale(index ? indexLanguage : language).humanize())} (${_p('article.word_count', words)})`;
                                 })()}
                             </span> : null}
                             {/* Visitor counter */}
