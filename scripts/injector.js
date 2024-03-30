0b69d5eebd76d89ac4565c8352eb24ae7293d219
xiang578 <xiang578@foxmail.com>
2024-03-30 13:39:46 +0800

    update

--- scripts/injector.js
+++ scripts/injector.js
@@ -6,6 +6,7 @@ hexo.extend.filter.register('theme_inject', function (injects) {
 hexo.extend.injector.register('head_begin', '<link rel="stylesheet" href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.6.0/lxgwwenkaiscreen.css">', 'default');
 
 hexo.extend.injector.register('head_begin', '<script async src="https://umami.xiang578.com/script.js" data-website-id="0bf958fd-ca98-4a54-b1c0-bf3384802fcc"></script>', 'default');
+hexo.extend.injector.register('head_begin', '<script data-goatcounter="https://xiang578.goatcounter.com/count" async src="/count.js"></script>', 'default');
 
 hexo.extend.injector.register('head_begin', '<link rel="webmention" href="https://webmention.io/blog.xiang578.com/webmention" /> <link rel="pingback" href="https://webmention.io/blog.xiang578.com/xmlrpc" />', 'default');
 hexo.extend.injector.register('head_begin', '<link title="Follow me on Twitter" rel="me" href="https://twitter.com/xiang578"/>', 'default');
